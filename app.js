948ffdb7a784896fc7023176c5ad6786108b3a0d
Juan Sebastian <71613539+juansbassi5@users.noreply.github.com>
2021-11-26 23:43:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 700px
+			  breakpoint: 700,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3
+			  }
+			}
+		]
+	});
+
+	
+});
